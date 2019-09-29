4314e16bc5cdbf051455deee4a95f8c8f44183aa
kivancsahici@yahoo.com <kivancsahici@yahoo.com>
2019-09-29 22:11:40 +0200

    implement tests

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,35 +1,86 @@
-/*import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AuthorQuiz  from './AuthorQuiz';
+import Enzyme, {mount, shallow, render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { exportAllDeclaration } from '@babel/types';
+Enzyme.configure({adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-*/
+const state = {
+  turnData:{
+    books:['The Shining', 'IT', 'David Copperfield', 'A Tale of Two Cities', 'Hamlet'],
+    author:{
+      name: 'Charles Dickens',
+      imageUrl: 'images/authors/charlesdickens.jpg',
+      imageSource:'Wikimedia Commons',
+      books:['David Copperfield', 'A Tale of Two Cities']
+    }
+  },
+  highlight: 'none'
+}
 
-describe("when setting up testing", () => {
-
-  let classroom = {
-    "employees":[
-      {"firstName":"Sally", "lastName":"Doe"},
-      {"firstName":"Anna", "lastName":"Smith"},
-      {"firstName":"Peter", "lastName":"Jones"}
-    ]
-    };
-
-    let  numbers = [45, 4, 9, 16, 25];
-
-  
-  it("should fail", () => {
-    let add = (x,y) => x + y;
-
-    const sum = classroom.employees.reduce((total, value) =>
-      total + value.firstName
-    , "");
-
-    console.log(sum);
-    expect(add(3,5)).toBe(8);
+describe ("Author Quiz", () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={()=>{}}/>, div);
   });
-});
+
+  describe("When no answer had been selected", ()=>{
+    let wrapper;
+    beforeAll(()=>{
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={()=>{}}/>);
+    });
+
+    it("should have no background color", ()=>{
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('');
+    })
+  });
+
+  describe("When the wrong answer had been selected", ()=>{
+    let wrapper;
+    beforeAll(()=>{
+      wrapper = mount(
+      <AuthorQuiz {...(Object.assign({}, state, {highlight:'wrong'}))} onAnswerSelected={()=>{}}/>
+      );
+    });
+
+    it("should have no background color", ()=>{
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('red');
+    })
+  });
+
+  describe("When the correct answer had been selected", ()=>{
+    let wrapper;
+    beforeAll(()=>{
+      wrapper = mount(
+      <AuthorQuiz {...(Object.assign({}, state, {highlight:'correct'}))} onAnswerSelected={()=>{}}/>
+      );
+    });
+
+    it("should have no background color", ()=>{
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('green');
+    })
+  });
+
+  describe("When the first answer has been selected", ()=>{
+    let wrapper;
+    const handleAnswerSelected = jest.fn();
+
+    beforeAll(()=>{
+      wrapper = mount(
+      <AuthorQuiz {...state} onAnswerSelected={handleAnswerSelected}/>
+      );
+
+      wrapper.find('.answer').first().simulate('click');
+    });
+
+    it("onAnswerSelected should be called", ()=>{
+      expect(handleAnswerSelected).toHaveBeenCalled();
+    })
+    
+    it("should receive The Shining", ()=>{
+      expect(handleAnswerSelected).toHaveBeenCalledWith('The Shining');
+    })
+  });
+})
+
